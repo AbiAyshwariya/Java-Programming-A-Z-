aa19b6095dec71a429528deae9773b0753631bd5
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2024-02-17 08:50:18 -0800

    Conditional or Ternary Operators

---
+++ Operators/UnaryOperator.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+class UnaryOperator{
+	public static void main(String args[]){
+		int a=10;
+		System.out.println("Before Unary Operation: "+a);
+		a++;
+		System.out.println("After Unary Operatiion: "+a);
+	}
+}
