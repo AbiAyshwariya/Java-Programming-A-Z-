2884cff03af2e43e83233422a11fec7329dcf2af
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2024-02-17 08:51:51 -0800

    Conditional or Ternary Operator

---
+++ Operators/ConditionalorTernary.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+class ConditionalorTernary{
+	public static void main(String args[]){
+		int a=25,b=35,c;
+		c=a>b?a:b;
+		System.out.println("The Greatest Number: "+c);
+	}
+}
