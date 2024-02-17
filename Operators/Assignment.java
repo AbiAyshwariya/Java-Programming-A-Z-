d16531badc18feb1c565b36259d4e25286ad9331
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2024-02-17 08:46:22 -0800

    Assignment Operators

---
+++ Operators/Assignment.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+
+class Assignment{
+	public static void main(String args[]){
+		int a=123;
+		System.out.println("Value of a: "+a);
+		a+=10;
+		System.out.println("Addition a+=10:"+a);
+		a-=10;
+		System.out.println("Subtraction a-=10:"+a);
+		a*=10;
+		System.out.println("Multiplication a*=10:"+a);
+		a/=10;
+		System.out.println("Division a/=10:"+a);
+		a%=10;
+		System.out.println("Modulus a%=10:"+a);
+	}
+}
