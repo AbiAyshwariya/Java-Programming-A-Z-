75251bdd35646d7349e2d61b338ed610543c7918
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2024-02-17 08:40:05 -0800

    TypeCasting in java

---
+++ Basic Concepts/TypeCasting.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+
+class TypeCasting{
+	public static void main(String args[]){
+		int a=10;
+		double b=a;
+		double d=25.5392;
+		int c=(int)d;
+		System.out.println("Int a: "+a);
+		System.out.println("Double b: "+b);
+		System.out.println("Int c: "+c);
+		System.out.println("Double d: "+d);
+	}
+}
