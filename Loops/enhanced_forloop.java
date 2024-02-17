5343c42272e03816197fdef44c659569c5473f29
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2024-02-17 09:11:57 -0800

    Enhanced for loop in java

---
+++ Loops/enhanced_forloop.java
@@ -0,0 +1,11 @@
+package JavaBase;
+
+public class enhanced_forloop {
+	public static void main(String args[]) {
+		int numbers[]= {10,20,30,40,50,60,70,80};
+		for(int n:numbers) {
+			System.out.println(n);
+		}
+	}
+
+}
