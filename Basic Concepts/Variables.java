a7a0a7eece1756e3b0be377b359fea15b4287106
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2024-02-17 08:39:27 -0800

    Variables in Java

---
+++ Basic Concepts/Variables.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+
+class Variables{
+	public static void main(String args[]){
+		String name="Abi";
+		int age=20;
+	    char gender='F';
+	    float height=147.45f;
+		boolean disability=false;
+		System.out.println("Name: "+name);
+		System.out.println("Age: "+age);
+		System.out.println("Gender: "+gender);
+		System.out.println("Height:"+height);
+		System.out.println("Disability: "+disability);
+	}
+}
