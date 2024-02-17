eccde95fe2b1ca42a5fbacb02ff3f4df8d46baee
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2024-02-17 08:54:50 -0800

    Bitwise Operators

---
+++ Operators/Bitwise.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+
+class Bitwise{
+	public static void main(String args[]){
+		int a=2;
+		int b=3;
+		System.out.println("Bitwise AND: "+(a&b));
+		System.out.println("Bitwise OR: "+(a|b));
+		System.out.println("Bitwise XOR: "+(a^b));
+		System.out.println("Bitwise NOT: "+(~a));
+	}
+}
+		
+		
