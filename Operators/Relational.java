d8800c177a68d49467f12d74845de8b8647c8676
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2024-02-17 08:47:37 -0800

    Relational Operators

---
+++ Operators/Relational.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+
+class Relational{
+	public static void main(String args[]){
+		int a=100,b=50;
+		System.out.println("Equal to: "+(a==b));
+		System.out.println("NOT Equal to: "+(a!=b));
+		System.out.println("Greater than: "+(a>b));
+		System.out.println("Lesser than: "+(a<b));
+		System.out.println("Greater than or equal to: "+(a>=b));
+		System.out.println("Lesser than or equal to: "+(a<=b));
+		
+	}
+}
+		
