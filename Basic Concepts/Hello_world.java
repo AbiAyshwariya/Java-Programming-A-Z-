3c944e66572cb0b1c61fced382f6e1cb548ebfd8
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2024-02-17 08:37:35 -0800

    Hello  world program in Java

--- Basic Concepts/Hello_world.java
+++ Basic Concepts/Hello_world.java
@@ -1,1 +1,6 @@
-
+import java.lang.*;
+public class Hello_world{
+  public static void main(String[] arg){
+    System.out.println("Hello World!!Welcome to the adventure of Java Programming");
+  }
+}
