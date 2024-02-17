d6c7d37a1d84e37c6a6f7e4d3ecfc82383ac80f0
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2024-02-17 08:46:57 -0800

    Logical Operators

---
+++ Operators/Logical.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+class Logical{
+	public static void main(String args[]){
+		int m1=65,m2=75;
+		System.out.println("And &&:"+(m1>=35&&m2>=35));
+		System.out.println("Or ||:"+(m1>=35||m2>=35));
+	}
+}
+		
