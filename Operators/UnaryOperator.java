75ee6e5139b37fcc0b85a58ecbb82b27c80336fb
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2024-02-17 08:53:55 -0800

    Unary operators

--- Operators/UnaryOperator.java
+++ Operators/UnaryOperator.java
@@ -3,7 +3,7 @@ import java.lang.*;
 class UnaryOperator{
 	public static void main(String args[]){
 		int a=10;
-		System.out.println("Before Unary Operation: "+a);
+		System.out.println("Before  Unary Operation: "+a);
 		a++;
 		System.out.println("After Unary Operatiion: "+a);
 	}
